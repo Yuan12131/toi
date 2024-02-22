95c183f580ceb6861dd6f8567625467d5e80e27c
leeyuan <dbdks32123@gmail.com>
2024-02-22 18:32:50 +0900

    Modal : 장소 검색 컴포넌트

---
+++ src/components/Place.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+
+interface PlaceSelectorProps {
+  selectedPlaces: any[];
+  setSelectedPlaces: React.Dispatch<React.SetStateAction<any[]>>;
+}
+
+const PlaceSelector: React.FC<PlaceSelectorProps> = ({selectedPlaces, setSelectedPlaces}) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSelect = (place: any) => {
+    // 선택된 장소 정보 처리
+    setSelectedPlaces((prevPlaces: any) => [...prevPlaces, place]);
+    setSearchValue("");
+  };
+
+  return (
+    <div>
+      <GooglePlacesAutocomplete
+        apiKey="AIzaSyCZy50I036EZDc-Lsu5EdEoUzdblq8WdHc"
+        minLengthAutocomplete={2}
+        autocompletionRequest={{ types: ["country", "locality"] }}
+        selectProps={{
+          onChange: handleSelect,
+          placeholder: "방문할 국가나 도시를 검색하세요",
+        }}
+      />
+
+      {selectedPlaces.length > 0 && (
+        <div>
+          <h2>선택된 도시 이름</h2>
+          <ul>
+            {selectedPlaces.map((place, index) => (
+              <li key={index}>{place.label}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PlaceSelector;
