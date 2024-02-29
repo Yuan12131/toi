4c1afd5a8f5db243c3278b8c0c69e69d4badeda9
leeyuan <dbdks32123@gmail.com>
2024-02-29 18:20:32 +0900

    플랜 페이지 css 모듈 import

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -2,9 +2,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import ModalBar from "@/components/ModalBar";
-import dotenv from "dotenv";
-
-dotenv.config();
+import styles from "@/styles/planner.module.scss"
 
 interface PlaceDetails {
   formatted_address: string;
@@ -27,7 +25,6 @@ const Planner = () => {
   const [response, setResponse] = useState<any>(null);
   const [detailResult, setDetailResult] = useState<any>(null);
 
-  const apiKey = process.env.PLACE_API_KEY;
 
   const handleModalSubmit = async (data: any) => {
     setModalData(data);
@@ -49,7 +46,7 @@ const Planner = () => {
       .join(", ");
 
     const question =
-      "다음 날짜, 장소, 테마를 기반으로 첫날부터 마지막날까지 일정을 JSON 형식으로 응답해주세요. 일정은 동선을 고려해서 일자마다 시간으로 나누어 최상위 객체를 날짜별 yyyy-mm-dd로 나누고, 배열 속 객체들로 이루어진 time, place_name으로 나타내주세요. 또한, place_name의 값은 실제로 존재하는 구글 플레이스에서 검색 가능한 장소 이름이며, 구글 리뷰가 많거나 인기 있는 장소 중심이어야 합니다.";
+      "다음 날짜, 여행지, 테마를 기반으로 첫날 일정부터 마지막날 일정까지 여행 일정을 짜서 JSON 형식으로 응답해주세요. 일정은 동선을 고려해서 일자마다 시간으로 나누어 최상위 객체를 날짜별 yyyy-mm-dd로 나누고, 배열 속 객체들로 이루어진 time, place_name으로 나타내주세요. 또한, place_name의 값은 실제로 존재하는 구글 플레이스에서 검색 가능한 장소의 공식 영문명이며, 구글 리뷰가 많거나 인기 있는 장소 중심이어야 합니다.";
 
     const combinedQuestion = `${question} 
     시작일: ${startDate}, 종료일: ${endDate}, 
@@ -110,7 +107,7 @@ const Planner = () => {
               name: item.result.name,
               formatted_address: item.result.formatted_address,
               photo:
-                item.result.photos && item.result.photos[0]?.html_attributions,
+                item.result.photos && item.result.photos[0]?.photo_reference,
             }));
 
             return resultData;
@@ -134,8 +131,10 @@ const Planner = () => {
     setIsOpen(false);
   };
 
+  const apiKey = process.env.PLACE_API_KEY;
+
   return (
-    <>
+    <div className={styles.main}>
       {isOpen && (
         <ModalBar
           onSubmit={handleModalSubmit}
@@ -145,45 +144,35 @@ const Planner = () => {
       )}
       <button onClick={handleOpenModal}>여행 정보 입력하기</button>
       <button onClick={handleAskQuestion}>여행 코스 확인하기</button>
-      <div className="ml-10">
-        <h2>답변:</h2>
-        {response &&
-          response.map((dateData: any, index: any) => (
-            <div key={index}>
-              <h2>{dateData[0]}</h2>
-              <ul>
-                {dateData[1].map((schedule: any) => (
-                  <li key={schedule.time}>
-                    {schedule.time} - {schedule.place_name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-      </div>
-      <div>
-        {detailResult &&
-          detailResult.map((placeList: any, index: number) => (
-            <div key={index}>
-              {placeList.map((item: any, innerIndex: number) => (
-                <div key={innerIndex}>
+      {response &&
+  response.map((dateData: any, index: any) => (
+    <div key={index}>
+      <h2>{dateData[0]}</h2>
+      <ul>
+        {dateData[1].map((schedule: any, scheduleIndex: number) => (
+          <li key={scheduleIndex}>
+            {schedule.time} - {schedule.place_name}
+            {detailResult &&
+              detailResult[scheduleIndex] &&
+              detailResult[scheduleIndex].map((item: any, itemIndex: number) => (
+                <div key={itemIndex}>
                   <p>{item.name}</p>
                   <p>{item.formatted_address}</p>
                   {item.photo && item.photo[0] && (
-                    <a
-                      href={item.photo[0]}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      View Photo
-                    </a>
+                    <img
+                      src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=200&photo_reference=${item.photo}&key=AIzaSyDpnJtXd385DDjiz4Ow0KFzAA05cUtd3nA`}
+                      alt="place"
+                    />
                   )}
                 </div>
               ))}
-            </div>
-          ))}
-      </div>
-    </>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ))}
+
+    </div>
   );
 };
 
