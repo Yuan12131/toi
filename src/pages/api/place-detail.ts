a1958def86e27e472b1bb5bab6692795d2142a0d
lee <dbdks32123@gmail.com>
2024-03-06 19:44:02 +0900

    route : place api  환경변수

--- src/pages/api/place-detail.ts
+++ src/pages/api/place-detail.ts
@@ -1,7 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const { placeName } = req.query;
@@ -11,23 +8,23 @@ export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   }
 
   const fields = ["photos", "formatted_address", "name", "rating"].join(",");
-  const apiKey = process.env.PLACE_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_PLACE_API_KEY;
 
-  
-  
   try {
-    const apiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&language=ko&key=${apiKey}`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&language=ko&key=${apiKey}
+`;
     const searchApiResponse = await fetch(apiUrl);
     const searchData = await searchApiResponse.json();
-    
+
     // 검색 결과에서 모든 장소의 place_id 추출
     const placeIds = searchData.candidates.map(
       (candidate: { place_id: any }) => candidate.place_id
-      );
-      
-      // 각 place_id에 대해 Place 세부 정보 API 호출
-      const placeDetailsPromises = placeIds.map(async (placeId: any) => {
-      const detailsApiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=${fields}&language=ko&key=${apiKey}`;
+    );
+
+    // 각 place_id에 대해 Place 세부 정보 API 호출
+    const placeDetailsPromises = placeIds.map(async (placeId: any) => {
+      const detailsApiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=${fields}&language=ko&key=${apiKey}
+      `;
       const detailsApiResponse = await fetch(detailsApiUrl);
       return detailsApiResponse.json();
     });
