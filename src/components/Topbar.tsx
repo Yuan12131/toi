d619153c9ff16b0d4a8f41c6b09b72ea3112345c
lee <dbdks32123@gmail.com>
2024-03-07 17:51:47 +0900

    Topbar : 로그인, 로그아웃 상태관리

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,10 +1,42 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "@/styles/topbar.module.scss";
 import Link from "next/link";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 function Topbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState<string | JwtPayload>('');
+
+  useEffect(() => {
+    const loadUserFromToken = () => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        const decodedToken = jwt.decode(token) as JwtPayload;
+
+        if (decodedToken) {
+          const userName = decodedToken.name;
+          setIsLoggedIn(true);
+          setName(userName);
+        }
+      } else {
+        setIsLoggedIn(false);
+        setName('');
+      }
+    };
+
+    loadUserFromToken();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    setName('');
+    window.location.href = '/';
+  };
+
   return (
     <div className={styles.topbar}>
       <div>
@@ -13,9 +45,18 @@ function Topbar() {
         </Link>
       </div>
       <div>
-        <Link href="/login">
-        LOGIN
-        </Link>
+      {isLoggedIn ? (
+          <>
+            <p>{`${name} 님`}</p>
+            <Link href="/mypage/myinfo">MYPAGE</Link>
+            <button onClick={handleLogout}>LOGOUT</button>
+          </>
+        ) : (
+          <>
+            <Link href="/signup">SIGN UP</Link>
+            <Link href="/login">LOGIN</Link>
+          </>
+        )}
       </div>
     </div>
   );
