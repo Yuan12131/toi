9cae63794bd10c51a7725f6de8ef223ab70c51a3
lee <dbdks32123@gmail.com>
2024-03-10 23:45:00 +0900

    chore

--- src/app/mypage/myplan/page.tsx
+++ src/app/mypage/myplan/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "@/styles/myplan.module.scss";
-import { Select } from "@nextui-org/select";
 
 
 interface DataItem {
