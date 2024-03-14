ccf481f13ae23ee03ac5379efd0bae3dada81d6c
lee <dbdks32123@gmail.com>
2024-03-14 09:39:00 +0900

    ID 중복확인 : DB 연결, 종료  추가

--- src/pages/api/checkDuplicate.ts
+++ src/pages/api/checkDuplicate.ts
@@ -2,20 +2,23 @@ import { pool, connectDB, closeDB } from "@/utils/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const { userId } = req.query;
+    connectDB()
+  try {
+    const { userId } = req.query;
 
-        const [rows] = await pool.query("SELECT * FROM Users WHERE userId = ?", [
-        userId,
-        ]);
+    const [rows] = await pool.query("SELECT * FROM Users WHERE userId = ?", [
+      userId,
+    ]);
 
-        if (rows) {
-            res.json({ isDuplicate: true });
-        } else  {
-            res.json({ isDuplicate: false });
-        }
-    } catch (error) {
-        console.error("Error checking duplicate:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (rows) {
+      res.json({ isDuplicate: true });
+    } else {
+      res.json({ isDuplicate: false });
     }
+  } catch (error) {
+    console.error("Error checking duplicate:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    closeDB(); // Connection should be released after using
+  }
 }
