1c66ec9f29bd7f762645e33b711c81abbff1d744
lee <dbdks32123@gmail.com>
2024-03-20 13:07:54 +0900

    MongoDB config

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,26 +1,24 @@
-require('dotenv').config(); // .env 파일의 환경 변수 로드
+const { MongoClient } = require("mongodb");
 
-const { MongoClient } = require('mongodb');
-
-const uri = process.env.MONGODB_URI; // MongoDB 서버 URI를 환경 변수로부터 가져옴
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI; // MongoDB 서버 URI를 환경 변수로부터 가져옴
 
 let db;
 
 export async function connectDB() {
   try {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(uri);
     await client.connect();
-    console.log('Connected to MongoDB');
-    db = client.db('toui'); // MongoDB에서 사용할 데이터베이스 선택
+    console.log("Connected to MongoDB");
+    db = client.db("toui"); // MongoDB에서 사용할 데이터베이스 선택
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 }
 
 export function getDB() {
   if (!db) {
-    throw new Error('Database not connected');
+    throw new Error("Database not connected");
   }
   return db;
 }
@@ -29,9 +27,9 @@ export async function closeDB() {
   try {
     if (db) {
       await db.client.close();
-      console.log('Database connection closed');
+      console.log("Database connection closed");
     }
   } catch (error) {
-    console.error('Error closing database connection:', error);
+    console.error("Error closing database connection:", error);
   }
 }
