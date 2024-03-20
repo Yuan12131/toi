fb0080696814aab63de38127a03078fbe4ad825e
lee <dbdks32123@gmail.com>
2024-03-20 13:07:33 +0900

    MariaDB -> MongoDB로 변경

--- src/pages/api/save-plans.ts
+++ src/pages/api/save-plans.ts
@@ -1,13 +1,14 @@
-import { pool, closeDB, connectDB } from "@/utils/db";
-import { NextApiRequest, NextApiResponse } from "next";
+import { connectDB, getDB, closeDB } from "@/utils/mongodb";
+const { ObjectId } = require("mongodb");
+
 import jwt from "jsonwebtoken";
 import { v4 } from "uuid";
 
-const secretKey = "yuan";
+const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" }); // Handle non-POST requests
+    return res.status(405).json({ error: "Method not allowed" });
   }
   const plans = req.body;
 
@@ -16,32 +17,34 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     return res.status(401).json({ error: "Authorization token required" });
   }
 
-  await connectDB()
+  await connectDB();
 
   try {
     const decodedToken = await verifyToken(token);
     const User_Index = decodedToken.User_Index;
+    const userIndex = new ObjectId(User_Index);
     const courseIndex = v4();
+
+    const db = getDB();
+    const travelPlansCollection = db.collection("TravelPlans");
 
     for (const plan of plans) {
       const { day, date, time, place_name, formatted_address, photo } = plan;
 
       try {
-        const results = await pool.execute(
-          `INSERT INTO TravelPlans (User_Index, courseIndex, day, date, time, place_name, formatted_address, photo, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())`,
-          [
-            User_Index,
-            courseIndex,
-            day,
-            date,
-            time,
-            place_name,
-            formatted_address,
-            photo,
-          ]
-        );
+        const result = await travelPlansCollection.insertOne({
+          userIndex,
+          courseIndex,
+          day,
+          date,
+          time,
+          place_name,
+          formatted_address,
+          photo,
+          timestamp: new Date(),
+        });
 
-        const insertedId = results.id;
+        const insertedId = result._id;
 
         res.json({ success: true, insertedId });
       } catch (error) {
@@ -52,16 +55,16 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   } catch (error) {
     console.error("Error verifying token:", error);
     res.status(401).json({ error: "Invalid or expired token" });
-  }finally {
-    closeDB(); // Connection should be released after using
-}
+  } finally {
+    closeDB();
+  }
 }
 
-async function verifyToken(token: string) {
+async function verifyToken(token) {
   try {
     const decoded = jwt.verify(token, secretKey);
     return decoded;
   } catch (error) {
     throw error;
-  } 
+  }
 }
