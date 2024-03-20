9a4a6ada1c182421148cd5f924ba78726e3f32da
lee <dbdks32123@gmail.com>
2024-03-20 13:07:37 +0900

    MariaDB -> MongoDB로 변경

--- src/pages/api/updateUser.ts
+++ src/pages/api/updateUser.ts
@@ -1,87 +1,91 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { pool, closeDB, connectDB } from "@/utils/db";
+import { connectDB, getDB, closeDB } from "@/utils/mongodb";
 import jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
 
-const secretKey = "yuan";
+const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
 
 export default async function PUT(req: NextApiRequest, res: NextApiResponse) {
-  connectDB()
-    try {
-      if (req.method === "PUT") {
-        // 인증된 사용자인지 확인
-        const token = req.headers.authorization?.replace("Bearer ", "");
-        if (!token) {
-          console.error("토큰이 제공되지 않았습니다.");
-          return res.status(401).json({ error: "토큰이 제공되지 않았습니다." });
-        }
+  await connectDB();
 
-        const decodedToken = jwt.verify(token, secretKey);
-        if (!decodedToken) {
-          console.error("토큰이 유효하지 않습니다.");
-        }
+  try {
+    if (req.method === "PUT") {
+      // 인증된 사용자인지 확인
+      const token = req.headers.authorization?.replace("Bearer ", "");
+      if (!token) {
+        console.error("토큰이 제공되지 않았습니다.");
+        return res.status(401).json({ error: "토큰이 제공되지 않았습니다." });
+      }
 
-        const userId = decodedToken.userId;
+      const decodedToken = jwt.verify(token, secretKey);
+      if (!decodedToken) {
+        console.error("토큰이 유효하지 않습니다.");
+        return res.status(401).json({ error: "토큰이 유효하지 않습니다." });
+      }
 
-        // 클라이언트에서 보낸 업데이트할 사용자 정보
-        const {
-          name,
-          birthdate,
-          phoneNumber,
-          email,
-          postcode,
-          address,
-          detailaddress,
-          gender,
-        } = req.body;
+      const user_Index = decodedToken.User_Index;
 
-        // 데이터베이스에서 사용자 정보 업데이트
-        const result = await pool.query(
-          "UPDATE Users SET name = ?, birthdate = ?, phoneNumber = ?, email = ?, postcode = ? , address = ?, detailaddress = ? , gender = ? WHERE userId = ?",
-          [
+      // 클라이언트에서 보낸 업데이트할 사용자 정보
+      const {
+        name,
+        birthdate,
+        phoneNumber,
+        email,
+        postcode,
+        address,
+        detailaddress,
+        gender,
+      } = req.body;
+
+      const db = getDB();
+      const usersCollection = db.collection("users");
+
+      // 데이터베이스에서 사용자 정보 업데이트
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(user_Index) }, // ObjectId로 변환하여 사용자 조회
+        {
+          $set: {
             name,
-            new Date(birthdate),
+            birthdate: new Date(birthdate),
             phoneNumber,
             email,
             postcode,
             address,
             detailaddress,
             gender,
-            userId,
-          ]
-        );
-
-        if (result.affectedRows === 1) {
-          // 업데이트 성공 시 새로운 토큰 발급
-          const newToken = jwt.sign(
-            {
-              userId,
-              name,
-              birthdate,
-              phoneNumber,
-              email,
-              postcode,
-              address,
-              detailaddress,
-              gender,
-              cash: decodedToken.cash, // 이 부분은 사용자 정보에 따라 추가 또는 수정해야 할 수 있습니다.
-            },
-            secretKey,
-            { expiresIn: "1h" }
-          );
-          const newDecodedToken = jwt.verify(newToken, secretKey);
-          res.status(200).json({ token: newToken });
-        } else {
-          console.error("사용자를 찾을 수 없습니다.");
-          res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+          },
         }
+      );
+
+      if (result.matchedCount === 1) {
+        // 업데이트 성공 시 새로운 토큰 발급
+        const newToken = jwt.sign(
+          {
+            name,
+            birthdate,
+            phoneNumber,
+            email,
+            postcode,
+            address,
+            detailaddress,
+            gender,
+          },
+          secretKey,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ token: newToken });
       } else {
-        console.error("허용되지 않은 메서드");
-        res.status(405).json({ error: "허용되지 않은 메서드" });
+        console.error("사용자를 찾을 수 없습니다.");
+        res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
       }
-    } catch (error) {
-      console.error("Error updating user info:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    } finally {
-      closeDB(); // Connection should be released after using
+    } else {
+      console.error("허용되지 않은 메서드");
+      res.status(405).json({ error: "허용되지 않은 메서드" });
+    }
+  } catch (error) {
+    console.error("Error updating user info:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  } finally {
+    closeDB();
   }
-  };
+}
